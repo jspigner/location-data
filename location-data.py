7cb7fcbf4988838db7d9ab6e7037c79cc101a4e8
Julie Spigner <77714712+jspigner@users.noreply.github.com>
2021-07-29 21:06:05 -0700

    Add files via upload

---
+++ location-data.py
@@ -0,0 +1,8 @@
+# Create location data
+# Store values in variables
+
+country = "USA"
+state = "Florida"
+city = "Miami"
+continent = "North America"
+print(city + ", " + state + ", " + country + " is in " + continent + ".")
